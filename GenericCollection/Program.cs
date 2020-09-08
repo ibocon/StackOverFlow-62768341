1b68bde96eb136ca8deae5d46f86bfc911a73168
Yegun Kim <wlfka102@gmail.com>
2020-09-08 15:23:20 +0900

    work around

--- GenericCollection/Program.cs
+++ GenericCollection/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCollection
 {
+    interface IBaseViewModel<out T> where T : BaseModel { }
+
+    abstract class BaseModel { }
+
+    abstract class BaseViewModel<T> : IBaseViewModel<T> where T : BaseModel 
+    {
+        protected T model;
+    }
+
+    class ModelA : BaseModel { }
+
+    class ViewModelA : BaseViewModel<ModelA> { }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var collection = new List<IBaseViewModel<BaseModel>>();
+            collection.Add(new ViewModelA());
         }
     }
 }
