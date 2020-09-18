17a14399489bae0513bc909d0cc52d4fd38af3e6
Yegun Kim <wlfka102@gmail.com>
2020-09-18 13:46:51 +0900

    not solved

--- EditorFocusManipulation/EditorFocusManipulation/MyView.xaml.cs
+++ EditorFocusManipulation/EditorFocusManipulation/MyView.xaml.cs
@@ -19,63 +19,31 @@ namespace EditorFocusManipulation
             IsVisible = false;
         }
 
-        private async void OnEditorUnfocused(object sender, FocusEventArgs e)
+        private void OnEditorUnfocused(object sender, FocusEventArgs e)
         {
-            if (IsVisible)
-            {
-                await FocusEditorAsync();
-                Console.WriteLine($"<Unfocused> Editor: Focused {Editor.IsFocused}");
-            }
-            else
-            {
-                await UnfocusEditorAsync();
-            }
-        }
-
-        private async Task FocusEditorAsync()
-        {
-            Console.WriteLine($"<FocusEditor> START {Editor.IsFocused}");
-
-            while (!Editor.IsFocused)
-            {
-                Editor.Focus();
-                await Task.Delay(100);
-                Console.WriteLine($"<FocusEditor> PROGRESS {Editor.IsFocused}");
-            }
-            Console.WriteLine($"<FocusEditor> END {Editor.IsFocused}");
-        }
-
-        private async Task UnfocusEditorAsync()
-        {
-            Console.WriteLine($"<!UnfocusEditor> START {Editor.IsFocused}");
-            while (Editor.IsFocused)
+            if (!Editor.IsVisible)
             {
                 Editor.Unfocus();
-                await Task.Delay(100);
-                Console.WriteLine($"<!UnfocusEditor> PROGRESS {Editor.IsFocused}");
+                Console.WriteLine($"<Unfocused> Editor: Focused {Editor.IsFocused}");
             }
-
-            Console.WriteLine($"<!UnfocusEditor> END {Editor.IsFocused}");
         }
 
-        protected override void OnPropertyChanging([CallerMemberName] string propertyName = null)
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if (propertyName.Equals("IsVisible") && IsVisible)
+            if (propertyName == nameof(IsVisible))
             {
-                Console.WriteLine($"<IsVisibleChanging> InputView: IsVisible ({IsVisible}) -> {!IsVisible}");
-            }
-
-            base.OnPropertyChanging(propertyName);
-        }
-
-        protected override async void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            if (propertyName.Equals("IsVisible") && IsVisible)
-            {
+                if (IsVisible)
+                {
+                    Editor.Focus();
+                    Editor.Unfocused += OnEditorUnfocused;
+                }
+                else
+                {
+                    Editor.Unfocused -= OnEditorUnfocused;
+                    Editor.Unfocus();
+                }
                 Console.WriteLine($"<IsVisibleChanged> InputView: IsVisible {!IsVisible} -> ({IsVisible})");
-                await FocusEditorAsync();
             }
-
             base.OnPropertyChanged(propertyName);
         }
     }
