172ecf458cf6f7a05f803edd09010e1e4784705d
Yegun Kim <wlfka102@gmail.com>
2020-10-16 18:23:10 +0900

    update

---
+++ CustomizeView/ChildView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CustomizeView
+{
+    public partial class ChildView : ContentView
+    {
+        public static BindableProperty TextProperty =
+            BindableProperty.Create(
+                propertyName: nameof(Text),
+                returnType: typeof(string),
+                declaringType: typeof(ChildView),
+                defaultValue: string.Empty,
+                propertyChanged: (b, o, n) =>
+                {
+                    (b as ChildView).Label.Text = (string)n;
+                });
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public ChildView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CustomizeView/ParentView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CustomizeView
+{
+    public partial class ParentView : ContentView
+    {
+        public static BindableProperty ChildProperty =
+            BindableProperty.Create(
+                propertyName: nameof(Child),
+                returnType: typeof(ChildView),
+                declaringType: typeof(ParentView),
+                defaultValue: null,
+                propertyChanged: (b, o, n) =>
+                {
+                    (b as ParentView).Child = (ChildView)n;
+                });
+        public ChildView Child
+        {
+            get => (ChildView)GetValue(ChildProperty);
+            set => SetValue(ChildProperty, value);
+        }
+
+        public ParentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
