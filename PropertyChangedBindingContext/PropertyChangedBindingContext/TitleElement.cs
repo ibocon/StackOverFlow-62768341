b240242a226691892e9cd7adb4a83c60907d9188
Yegun Kim <wlfka102@gmail.com>
2020-11-30 19:13:36 +0900

    Binding Success

--- PropertyChangedBindingContext/PropertyChangedBindingContext/TitleElement.cs
+++ PropertyChangedBindingContext/PropertyChangedBindingContext/TitleElement.cs
@@ -6,7 +6,7 @@ namespace PropertyChangedBindingContext
     static class TitleElement
     {
         public static readonly BindableProperty TitleProperty =
-            BindableProperty.Create(nameof(ITitleElement.Title), typeof(string), typeof(ITitleElement), string.Empty, propertyChanged: OnTitleChanged);
+            BindableProperty.Create(nameof(ITitleElement.Title), typeof(string), typeof(ITitleElement), string.Empty, BindingMode.TwoWay, propertyChanged: OnTitleChanged);
         public static void OnTitleChanged(BindableObject bindable, object oldTitle, object newTitle)
         {
             (bindable as ITitleElement).OnTitleChanged(newTitle as string);
