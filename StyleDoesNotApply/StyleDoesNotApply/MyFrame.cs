0e316ce397fb3c1dfacc0ce964bc4775e2d537e4
Yegun Kim <wlfka102@gmail.com>
2020-12-22 15:02:26 +0900

    Simplify code.

--- StyleDoesNotApply/StyleDoesNotApply/MyFrame.cs
+++ StyleDoesNotApply/StyleDoesNotApply/MyFrame.cs
@@ -11,30 +11,12 @@ namespace StyleDoesNotApply
 
         public MyFrame()
         {
-            HorizontalOptions = LayoutOptions.Start;
-            VerticalOptions = LayoutOptions.CenterAndExpand;
-
-            MinimumWidthRequest = 82;
-            WidthRequest = 82;
-
-            MinimumHeightRequest = 38;
-            HeightRequest = 38;
-
-            Padding = new Thickness(0);
-            HasShadow = false;
-
-            CornerRadius = 20;
             BackgroundColor = Color.LightPink;
 
             _title = new Label
             {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                VerticalTextAlignment = TextAlignment.Center,
                 TextColor = Color.Black,
                 FontSize = 16,
-                Text = "Title",
             };
 
             Content = _title;
