35fa7baebfae782d2d5b9f3986b62d969f34fe82
Yegun Kim <wlfka102@gmail.com>
2020-12-22 14:58:29 +0900

    Reproductable project

---
+++ StyleDoesNotApply/StyleDoesNotApply/MyFrame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace StyleDoesNotApply
+{
+    public class MyFrame : Frame
+    {
+        private readonly Label _title;
+        public string Title { set => _title.Text = value; }
+
+        public MyFrame()
+        {
+            HorizontalOptions = LayoutOptions.Start;
+            VerticalOptions = LayoutOptions.CenterAndExpand;
+
+            MinimumWidthRequest = 82;
+            WidthRequest = 82;
+
+            MinimumHeightRequest = 38;
+            HeightRequest = 38;
+
+            Padding = new Thickness(0);
+            HasShadow = false;
+
+            CornerRadius = 20;
+            BackgroundColor = Color.LightPink;
+
+            _title = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                TextColor = Color.Black,
+                FontSize = 16,
+                Text = "Title",
+            };
+
+            Content = _title;
+        }
+    }
+}
+
---
+++ StyleDoesNotApply/StyleDoesNotApply/MyView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Xamarin.Forms;
+
+namespace StyleDoesNotApply
+{
+    public class MyView : ContentView
+    {
+        private readonly StackLayout _layout;
+        public ObservableCollection<View> Views { get; set; }
+
+        public MyView()
+        {
+            Views = new ObservableCollection<View>();
+            Views.CollectionChanged += OnViewsCollectionChanged;
+
+            _layout = new StackLayout();
+            Content = _layout;
+        }
+
+        private void OnViewsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+                foreach (View view in e.OldItems)
+                    _layout.Children.Remove(view);
+
+            if (e.NewItems != null)
+                foreach (View view in e.NewItems)
+                    _layout.Children.Add(view);
+        }
+    }
+}
+
